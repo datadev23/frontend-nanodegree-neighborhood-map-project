041c46491f9907552ce403c05f3570f6dca3e2a1
maindev <systemdevcloud@live.co.uk>
2016-09-18 14:47:09 +0100

    ko return of google coordinates

---
+++ js/models/model.js
@@ -0,0 +1,38 @@
+  
+  
+
+/* http://knockoutjs.com/documentation/custom-bindings.html */
+function MyModel() {
+
+this.mapOne = ko.observable({
+        lat: ko.observable(40.74135),
+        lng:ko.observable(-73.99802)
+    });
+
+	
+
+}
+
+
+
+$(document).ready(function () {
+   ko.applyBindings(model);
+});
+ 
+
+
+var model = new MyModel();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
